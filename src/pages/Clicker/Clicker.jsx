8a5027764f63faabd495a5556b3119d83ee3a1fe
Akudama <urlyapov17@bk.ru>
2025-07-25 13:43:18 +0300

    test gaps clicker

--- src/pages/Clicker/Clicker.jsx
+++ src/pages/Clicker/Clicker.jsx
@@ -2,28 +2,27 @@ import React from 'react';
 import classes from './Clicker.module.css';
 import wrappers from "../PagesStyles.module.css";
 import BottomNav from '../../components/layout/BottomNav/BottomNav';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
 import ClickerStar from '../../assets/images/ClickerStar.png';
+import Light from '../../assets/images/Light.png';
 
 const Clicker = () => {
   return (
     <div className={`${wrappers.Wrapper} ${classes.clickerBackground}`}>
-      <div className={classes.header}>
-        <i></i>
-        <span>2 lvl</span>
-      </div>
       <div className={wrappers.content}>
         <div className={classes.actionCount}>
-          <FontAwesomeIcon icon={faStar}/>
-          <span>2,005</span>
+          <img className={classes.starInfo} src={ClickerStar} alt='count star'/>
+          <span className={classes.starsInfo}>2,005</span>
         </div>
         <div className={classes.actionTap}>
-          <img className={classes.star} src={ClickerStar} alt='clicker star' />
+          <button className={classes.actionBtn}>
+            <img className={classes.star} src={ClickerStar} alt='clicker star'/>
+          </button>
         </div>
         <div className={classes.actionStamina}>
-          <i></i>
-          <span>100/100</span>
+          <div className={classes.staminaWrapper}>
+            <img className={classes.light} src={Light} alt='light'/>
+            <span className={classes.stamina}>2000 / 2000</span>
+          </div>
           <progress 
             value={100}
             max={100}
