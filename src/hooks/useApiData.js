c9a3ccbbd8dde520af04fa2413a26e644add6101
Akudama <urlyapov17@bk.ru>
2025-07-28 22:03:06 +0300

    dfkfksdfdk

--- src/hooks/useApiData.js
+++ src/hooks/useApiData.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'https://solid-fishstick-j649vqq6wxw2q7jq-80.app.github.dev';
+const API_URL = 'https://organic-orbit-pvg94ppvvrjf66wx-80.app.github.dev';
 
 const useFetchData = (endpoint) => {
   const [data, setData] = useState(null);
