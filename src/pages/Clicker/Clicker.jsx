23d2c1c2b4929210943238cbad860e5e1c8636f5
Akudama <urlyapov17@bk.ru>
2025-07-30 01:23:53 +0300

    new states added

--- src/pages/Clicker/Clicker.jsx
+++ src/pages/Clicker/Clicker.jsx
@@ -99,6 +99,7 @@ const Clicker = () => {
               <button className={`${classes.actionBtn} ${isPressed ? classes.pressed : ''}`} 
                 onTouchStart={handlePressStart}
                 onTouchEnd={handlePressEnd}
+                onClick={handlePressStart}
               >
               <img className={classes.star} src={ClickerStar} alt='clicker star' draggable="false"/>
             </button>
