8d0425fd975013bdf89b052eab22bce2b0e355d8
Akudama <urlyapov17@bk.ru>
2025-07-30 14:39:35 +0300

    Saving after closing the window

--- src/pages/Clicker/Clicker.jsx
+++ src/pages/Clicker/Clicker.jsx
@@ -25,8 +25,8 @@ const Clicker = () => {
 
     const newParticles = Array(5).fill().map(() => ({
       id: Math.random().toString(36).substring(2, 9),
-      x: Math.random() * 250 - 100, 
-      y: Math.random() * 250 - 100, 
+      x: Math.random() * 250 - 150, 
+      y: Math.random() * 250 - 150, 
       size: Math.random() * 20 + 5,
       createdAt: Date.now(),
     }));
@@ -58,7 +58,7 @@ const Clicker = () => {
       setParticles(prev => 
         prev.filter(p => Date.now() - p.createdAt < 1000) 
       );
-    }, 300); 
+    }, 700); 
 
   return () => clearInterval(interval); 
   })
