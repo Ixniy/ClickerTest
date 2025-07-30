419a0a864941ee4bb12fdd0b4d2bcaec9fa79923
Akudama <urlyapov17@bk.ru>
2025-07-30 16:10:39 +0300

    Saving after closing the window

--- src/pages/Clicker/Clicker.jsx
+++ src/pages/Clicker/Clicker.jsx
@@ -6,7 +6,6 @@ import Light from '../../assets/images/Light.png';
 import {putData} from '../../services/putFetchData';
 import { useTelegram } from '../../hooks/useTelegram';
 import useApiData from '../../hooks/useApiData';
-import { newParticles } from '../../utils/fontawesome';
 // import API_URL from '../../hooks/useApiData';
 
 const Clicker = () => {
@@ -24,8 +23,14 @@ const Clicker = () => {
   const handlePressStart = () => {
     if (localData.energy <= 0) return;
 
-    const starsParticles = newParticles;
-    setParticles(prev => [...prev, ...starsParticles]);
+    const newParticles = Array(5).fill().map(() => ({
+      id: Math.random().toString(36).substring(2, 9),
+      x: Math.random() * 250 - 150, 
+      y: Math.random() * 250 - 150, 
+      size: Math.random() * 20 + 5,
+      createdAt: Date.now(),
+    }));
+    setParticles(prev => [...prev, ...newParticles]);
 
     setTimeout(() => setParticles([]), 1000);
 
--- src/utils/fontawesome.js
+++
@@ -1,8 +0,0 @@
-export const newParticles = Array(5).fill().map(() => ({
-
-    id: Math.random().toString(36).substring(2, 9),
-    x: Math.random() * 250 - 150, 
-    y: Math.random() * 250 - 150, 
-    size: Math.random() * 20 + 5,
-    createdAt: Date.now(),
-}));
