daf34068dd91c2316e31b5e1bc5b976c9a0889c8
Akudama <urlyapov17@bk.ru>
2025-07-30 17:03:07 +0300

    Saving after closing the window

--- src/pages/Clicker/Clicker.jsx
+++ src/pages/Clicker/Clicker.jsx
@@ -121,15 +121,15 @@ const Clicker = () => {
   //   window.addEventListener('unload', forceSyncOnExit);
   // }, [user.id, tg, onClose])
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setParticles(prev => 
-        prev.filter(p => Date.now() - p.createdAt < 1000) 
-      );
-    }, 300); 
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setParticles(prev => 
+  //       prev.filter(p => Date.now() - p.createdAt < 1000) 
+  //     );
+  //   }, 300); 
 
-  return () => clearInterval(interval); 
-  });
+  // return () => clearInterval(interval); 
+  // });
 
 
   if (loading) return <div>Загрузка...</div>;
